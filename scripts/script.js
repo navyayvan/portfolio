1a85fa05e07fddafaddfc0c495165355bf25196a
navyayvan <navyayvan@gmail.com>
2016-03-31 22:12:40 -0700

    some styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,48 @@
 $(document).ready(function() {
-	console.log('Running');
 
-	$('.tlt').textillate({
-		selector: '.textanimate',
-		loop: true,
-		minDisplayTime: 2000,
-		initialDelay: 0,
-		autoStart: true
+	var scroll = function(where){
+        $('html, body').animate({
+            scrollTop: where.offset().top
+        }, 500);
+    };
+
+    $('.tlt').textillate({
+        loop: true,
+        minDisplayTime: 1000,
+        initialDelay: 0,
+        autoStart: true
+    });
+
+  $('#about').on('click', function(){
+      $('.about').removeClass('hide').addClass('show');
+      $('.work').removeClass('show').addClass('hide');
+      $('.hobbies').removeClass('show').addClass('hide');
+      scroll($('#buffer'));
+      $('#up').removeClass('hide');
+  });
+
+  $('#work').on('click', function(){
+      $('.work').removeClass('hide').addClass('show');
+      $('.about').removeClass('show').addClass('hide');
+      $('.hobbies').removeClass('show').addClass('hide');
+      scroll($('#buffer'));
+      $('#up').removeClass('hide');
+  });
+
+  $('#hobbies').on('click', function(){
+      $('.hobbies').removeClass('hide').addClass('show');
+      $('.about').removeClass('show').addClass('hide');
+      $('.work').removeClass('show').addClass('hide');
+      scroll($('#buffer'));
+      $('#up').removeClass('hide');
+  });
+
+  $('#up').on('click', function(){
+    $('.work').removeClass('show').addClass('hide');
+    $('.about').removeClass('show').addClass('hide');
+    $('.hobbies').removeClass('show').addClass('hide');
+    $('#up').addClass('hide');
 	});
-})
+
+	
+});
